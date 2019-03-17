28eb8c5b5d40bbf5e144f72f7281c96a1f8f168e
Holden <holdenjr@egb070000037.lancs.local>
2019-03-17 20:45:45 +0000

    Added a black and white button to the frame.

---
+++ Draughts_Board.java
@@ -0,0 +1,34 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class Draughts_Board
+{
+        JFrame board = new JFrame("Draughts");
+        JPanel panel = new JPanel();
+        ImageIcon white = new ImageIcon("empty1.png");
+        JButton b = new JButton(white);
+        ImageIcon black = new ImageIcon("empty2.png");
+        JButton w = new JButton(black);
+
+    public Draughts_Board()
+    {
+        board.add(panel);
+        panel.add(b);
+        panel.add(w);
+        b.setSize(100,100);
+        w.setSize(100,100);
+        board.setSize(800,800);
+        panel.setSize(800,800);
+        board.setTitle("Draughts!");
+        board.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        board.setVisible(true);
+        panel.setVisible(true);
+    }
+
+    public static void main(String[] args)
+    {
+        Draughts_Board board = new Draughts_Board();
+    }
+
+}
---
+++ Draughts_Square.java
@@ -0,0 +1,19 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class Draughts_Square
+{
+
+    public Draughts_Board()
+    {
+
+
+    }
+
+    
+    public static void main(String[] args)
+    {
+
+    }
+}
