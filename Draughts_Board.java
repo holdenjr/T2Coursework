498d3dc8515216847ebe1219e56139a26f0d80dc
Holden <holdenjr@egb070000037.lancs.local>
2019-03-17 22:01:02 +0000

    Created a checker-board using an array of JButtons.

--- Draughts_Board.java
+++ Draughts_Board.java
@@ -4,14 +4,20 @@ import java.awt.event.*;
 
 public class Draughts_Board
 {
-        JFrame board = new JFrame("Draughts");
-        JPanel panel = new JPanel();
+        private JFrame board = new JFrame("Draughts");
+        private JPanel panel = new JPanel();
+
         ImageIcon white = new ImageIcon("empty1.png");
-        JButton b = new JButton(white);
         ImageIcon black = new ImageIcon("empty2.png");
-        JButton w = new JButton(black);
+
         GridLayout grid = new GridLayout(8,8);
 
+        //JButton b = new JButton(white);
+        //JButton w = new JButton(black);
+
+        private JButton[][]buttons;
+        private final int SIZE = 8;
+ 
     public Draughts_Board()
     {
         board.add(panel);
@@ -20,17 +26,37 @@ public class Draughts_Board
 
         panel.setLayout(grid);
 
-        panel.add(b);
-        panel.add(w);
-        b.setSize(100,100);
-        w.setSize(100,100);
-
+        //panel.add(b);
+        //panel.add(w);
+        //b.setSize(100,100);
+        //w.setSize(100,100);
+        buttons = new JButton[SIZE][SIZE];
+        addButtons();
 
         board.setTitle("Draughts!");
         board.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         board.setVisible(true);
-        
+
         panel.setVisible(true);
+        
+    }
+
+    public void addButtons()
+    {
+        for (int a = 0; a < SIZE; a++)
+        {
+            for (int b = 0; b < SIZE; b++)
+            {
+                if((a+b)%2 != 0){
+                    buttons[a][b] = new JButton(white);
+                    panel.add(buttons[a][b]);
+                }
+                else{
+                    buttons[a][b] = new JButton(black);
+                    panel.add(buttons[a][b]);
+                }
+            }
+        }    
     }
 
     public static void main(String[] args)
