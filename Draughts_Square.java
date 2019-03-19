99c8f51e4fa1ed38e458a86cb8ad55d5cf95cba5
Holden <holdenjr@egb070000045.lancs.local>
2019-03-19 12:19:01 +0000

    Declared variables in the Square.

--- Draughts_Square.java
+++ Draughts_Square.java
@@ -5,8 +5,40 @@ import java.awt.event.*;
 public class Draughts_Square
 {
 
+
+    ImageIcon white = new ImageIcon("empty1.png");                                   //Sets the variable "white" to the Empty1 image.
+    ImageIcon black = new ImageIcon("empty2.png"); 
+    ImageIcon redPiece = new ImageIcon("red.png");
+    ImageIcon redKing = new ImageIcon("red-king.png");
+    ImageIcon whitePiece = new ImageIcon("white.png");
+    ImageIcon whiteKing = new ImageIcon("white-king.png");
+    JButton whiteP = new JButton(white);
+    JButton blackP = new JButton(black);
+    JButton selected = new JButton(selected);
+
     public Draughts_Board()
     {
+        xpos = x;
+        ypos = y;
+
+        if piece white
+        white seticon white
+        else if piece red
+        white seticon redp
+
+
+    }
+
+
+    public JButton getwhite
+    {
+        return whiteP;
+    }
+    
+
+    public JButton getblack
+    {
+        return blackP;
     }
 
     public static void main(String[] args)
