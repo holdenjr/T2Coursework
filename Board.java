e993f0ffd5faaaa45eba4938483223458449c166
Holden <holdenjr@egb070000016.lancs.local>
2019-03-21 20:44:00 +0000

    Continued to work on OOP process.

--- Board.java
+++ Board.java
@@ -2,53 +2,44 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 
-public class Board extends Square
+public class Board
 {
-        JFrame board = new JFrame("Draughts");                                   //Creates the Frame, and names it Draughts.
-        JPanel panel = new JPanel();                                             //Creates the panel.
-
-        GridLayout grid = new GridLayout(8,8);                                           //Declares the grid layout of 8x8.
-
-        JButton[][]buttons;                                                      //Creates the button array.
-
-    public Board()
-    {
-        board.add(panel);                                                                //Adds the panel to the board.
-        board.setSize(800,800);                                                          //Sets the size of the board.
-        panel.setSize(800,800);                                                          //Sets the size of the panel.
-
-        panel.setLayout(grid);                                                           //Sets the panel to the layout of "grid".
-
-        Square[][] buttons = new Square[SIZE][SIZE];                                               //Sets the size of the "buttons" array.
-        addButtons();
-
-        board.setTitle("Draughts!");                                                     //Sets the title of the board as "Draughts!".
-        board.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);                            //Closes the board upon exit.
-        board.setVisible(true);                                                          //Sets the board so that it is visible.
-
-        panel.setVisible(true);                                                          //Sets the panel so that it is visible.  
-    }
-
-    public void addButtons()
-    {
-        for (int a = 0; a < SIZE; a++)                                                   //When a is less than 8, add one.
-        {
-            for (int b = 0; b < SIZE; b++)                                               //When b is less than 8, add one.
-            {
-                if((a+b)%2 != 0){                                                        //If a + b remainder 2 is not equal to 0.
-                    buttons[a][b] = new JButton(white);                                       //Add a new white button.
-                    panel.add(buttons[a][b]);
-                }
-                else{
-                    buttons[a][b] = new JButton(black);                                  //Otherwise, add a black button.
-                    panel.add(buttons[a][b]);
-                }               
-            }
-        }
-    }
-
     public static void main(String[] args)
     {
-        Board board = new Board();                                    //This creates the board.        
+        JFrame board = new JFrame("Draughts");
+        JPanel panel = new JPanel();
+
+        GridLayout grid = new GridLayout(8,8);
+        board.add(panel);
+        board.setSize(800,800);
+        panel.setSize(800,800);
+        panel.setLayout(grid);
+
+        final int SIZE = 8;
+
+        Square[][] buttons = new Square[SIZE][SIZE];
+
+        board.setTitle("Draughts!");
+        board.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        board.setVisible(true);
+
+        panel.setVisible(true); 
+
+        for (int a = 0; a < SIZE; a++)
+        {
+            for (int b = 0; b < SIZE; b++)
+            {
+                if((a+b)%2 != 0)
+                {
+                    buttons[a][b] = new Square(a, b, false);
+                    panel.add(buttons[a][b].getButton());
+                }
+                else
+                {
+                    buttons[a][b] = new Square(a, b, true);
+                    panel.add(buttons[a][b].getButton());
+                }               
+            }
+        }  
     }
 }
--- Square.java
+++ Square.java
@@ -1,9 +1,15 @@
+import javax.lang.model.util.ElementScanner6;
 import javax.swing.*;
+import javax.swing.plaf.basic.BasicScrollPaneUI.ViewportChangeHandler;
+
 import java.awt.*;
 import java.awt.event.*;
 
 public class Square
 {
+    int xPosition;
+    int yPosition;
+
     ImageIcon white = new ImageIcon("empty1.png");
     ImageIcon black = new ImageIcon("empty2.png");
     ImageIcon selectedSquare = new ImageIcon("selected.png");
@@ -11,25 +17,27 @@ public class Square
     ImageIcon redKing = new ImageIcon("red-king.png");
     ImageIcon whitePiece = new ImageIcon("white.png");
     ImageIcon whiteKing = new ImageIcon("white-king.png");
+    JButton whiteSquare = new JButton(white);
+    JButton blackSquare = new JButton(black);
+    boolean isWhite;
 
-    final int SIZE = 8; 
-
-    public Square()
+    public  Square(int x, int y, boolean isW)
     {
-       /* for (int a = 0; a < SIZE; a++)                                                   //When a is less than 8, add one.
+        xPosition = x;
+        yPosition = y;
+        isWhite = isW;
+    }
+
+    public JButton getButton()
+    {
+        if (isWhite == true)
         {
-            for (int b = 0; b < SIZE; b++)                                               //When b is less than 8, add one.
-            {
-                if((a+b)%2 != 0){                                                        //If a + b remainder 2 is not equal to 0.
-                    buttons[a][b] = new JButton();                                       //Add a new white button.
-                    panel.add(buttons[a][b]);
-                }
-                else{
-                    buttons[a][b] = new JButton();                                  //Otherwise, add a black button.
-                    panel.add(buttons[a][b]);
-                }               
-            }
-        }*/
+            return whiteSquare;
+        }
+        else
+        {
+            return blackSquare;
+        }
     }
 
     public static void main(String[] args)
