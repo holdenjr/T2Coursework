3650051240179f844efa02c3cff0a6a2d20268be
Holden <holdenjr@egb070000016.lancs.local>
2019-03-22 14:10:31 +0000

    Added javadoc commenting to documents and generated necessary documentation.

--- Board.java
+++ Board.java
@@ -1,13 +1,13 @@
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
-
-
 /**
+*@author Jack Holden - 34725091
+*@version 1.0
  * Represents the board of the game "Draughts"
  * where the GUI is displayed, creating the
  * board.
- */
+ **/
 public class Board
 {
     public Board()
@@ -15,37 +15,52 @@ public class Board
         /**
          * Creates a new JFrame for the game, and a 
          * new JPanel.
-         */
+         **/
         JFrame board = new JFrame("Draughts");
         JPanel panel = new JPanel();
 
         /**
          * Sets a grid layout with the name "grid".
-         * @param 8 This is the x-coordinate.
-         * @param 8 This is the y-coordinate.
-         */
+         **/
         GridLayout grid = new GridLayout(8,8);
 
         /**
          * This sets the sizes of the board and panel, and sets 
          * the layout of the panel to the aforementioned grid.
-         * @param 800 sets the x-length of the board.
-         * @param 800 sets the y-length of the board.
-         * @param 800 sets the x-length of the panel.
-         * @param 800 sets the y-length of the panel.
-         */
+         **/
         board.setSize(800,800);
         panel.setSize(800,800);
         panel.setLayout(grid);
 
+        /**
+         * This sets the title of the board to "Draughts"
+         * and ensures that the GUI is closed when exited.
+         **/
         board.setTitle("Draughts");
         board.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
+        /**
+         * This sets the variable SIZE to 8.
+         **/
         final int SIZE = 8;
+
+        /**
+         * This creates a 2D array, called buttons calling
+         * variables from Square using the SIZE variable 
+         * declared previously.
+         **/
         Square[][] buttons = new Square[SIZE][SIZE];
 
+        /**
+         * This loop sets the variable a to 0, then as long as it is
+         * below SIZE (8), it will increment by 1.
+         **/
         for (int a = 0; a < SIZE; a++)
         {
+            /**
+             * This loop sets the b variable to 0, then as long as it is
+             * below SIZE (8), it will increment by 1.
+             **/
             for (int b = 0; b < SIZE; b++)
             {
                 if((a+b)%2 != 0)
@@ -59,13 +74,21 @@ public class Board
                     panel.add(buttons[a][b].getButton());
                 }               
             }
-        }  
+        } 
+        
+        /**
+        * This sets the board's content pane to Panel,
+        * and then sets the board's visibility to true.
+        **/
 
         board.setContentPane(panel);
         board.setVisible(true);
     }
     public static void main(String[] args)
     {
+        /**
+         * This creates the board.
+         */
         Board board = new Board();
     }  
 }
--- Square.java
+++ Square.java
@@ -3,15 +3,29 @@ import javax.swing.border.*;
 import java.awt.*;
 import java.awt.event.*;
 import java.awt.Point;
-
+/**
+*@author Jack Holden - 34725091
+*@version 1.0
+ * Represents the individual squares of the game "Draughts"
+ * which are then taken by the Board GUI.
+ **/
 public class Square implements ActionListener
 {
+    /**
+     * This creates and empty border for the buttons.
+     **/
     Border borderE = BorderFactory.createEmptyBorder();
 
+    /**
+     * This sets the variables to int.
+     **/
 	int xPosition;
     int yPosition;
-
-    ImageIcon white = new ImageIcon("empty1.png");
+    
+    /**
+     *This is where all of the variables are declared. 
+     **/
+    ImageIcon white = new ImageIcon("empty.png");
     ImageIcon black = new ImageIcon("empty2.png");
     ImageIcon selectedSquare = new ImageIcon("selected.png");
     ImageIcon redPiece = new ImageIcon("red.png");
@@ -23,56 +37,94 @@ public class Square implements ActionListener
     Point whitePosition = new Point();
     public boolean isWhite;
     public String pieceHere; 
-
     String redPieceHere = "RED";
     String whitePieceHere = "WHITE";
     String blankSpace = "NULL";
 
-public void actionPerformed(ActionEvent e)
-{
-    System.out.println(xPosition+" "+yPosition+" ");
-}
+    /**
+     * This calls the action performer
+     * @param e This allows the code to access the properties of "ActionEvent"
+     **/
+    public void actionPerformed(ActionEvent e)
+    {
+    /**
+     * When the action is performed, the position in the array is displayed.
+     **/
+        System.out.println(xPosition+" "+yPosition+" ");
+    }
 
+    /**
+     * This is the Square class, where squares are generated based on values.
+     * @param x This is the x-coordinate of the array.
+     * @param y This is the y-coordinate of the array.
+     * @param isW This is the boolean to decide if the position is a white square or not.
+     **/
     public Square(int x, int y, boolean isW)
     {
+        /**
+        * This sets various variables to a simpler, easier to type
+        * way for ease of use. 
+        **/
         xPosition = x;
         yPosition = y;
         isWhite = isW;
 
+        /**
+        * This sets the border of the buttons to borderless as declared above.
+        **/
         btnSquare.setBorder(borderE);
         btnPiece.setBorder(borderE);
 
+        /**
+        * This sets the square to white if isWhite is true. 
+        **/
         if (isWhite == true)
         {
             btnSquare.setIcon(white);
             whitePosition.setLocation(x,y);
 
+            /**
+             *If the y-coordinate is less than or equal to 2,
+             * then it sets the icon of the square to a red piece. 
+             **/
                 if (y <= 2)
                 {
                     pieceHere = redPieceHere;
                     btnSquare.setIcon(redPiece);
                 }
+                /**
+                *If the y-coordinate is greater than or equal to 5,
+                * then it sets the icon of the square to a white piece. 
+                **/
                 else if(y >= 5)
                 {
                     pieceHere = whitePieceHere;
                     btnSquare.setIcon(whitePiece);
                 }
+                /**
+                 * Otherwise the square is unchanged.
+                 **/
                 else
                 {
                     pieceHere = blankSpace;
                 } 
         }
+        /**
+         * Then the square is set to a black image.
+         **/
         else
         {
             btnSquare.setIcon(black);
         }
-
+        /**
+         * This sets up the actionListener on the btnSquare.
+         **/
         btnSquare.addActionListener(this);
-
-        int[][]pos1 = new int[x][y];
-        System.out.println(pos1);
     }
 
+    /**
+     * This is the return class for getButton.
+     **/
     public JButton getButton()
     {
         return btnSquare;
